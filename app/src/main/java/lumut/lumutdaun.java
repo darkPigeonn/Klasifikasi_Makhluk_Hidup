217e63b3e66d6f21ceb22f333204d3f4d08231b3
AtanasiusIvannoel <50091059+darkPigeonn@users.noreply.github.com>
2021-04-15 16:58:58 +0700

    yuhuuuu

---
+++ app/src/main/java/lumut/lumutdaun.java
@@ -0,0 +1,16 @@
+package lumut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.klasifikasimakhlukhidup.R;
+
+public class lumutdaun extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lumutdaun);
+    }
+}
---
+++ app/src/main/java/lumut/lumuthati.java
@@ -0,0 +1,16 @@
+package lumut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.klasifikasimakhlukhidup.R;
+
+public class lumuthati extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lumuthati);
+    }
+}
